26d1da77e37b51bb3d3f88fc90bc5fe2aa36f6d4
Nick Rodriguez <nickrrodriguez@gmail.com>
2022-01-16 02:02:53 -0500

    tailwind added - blog details page 40%

--- src/pages/BlogDetails/BlogDetails.jsx
+++ src/pages/BlogDetails/BlogDetails.jsx
@@ -5,38 +5,68 @@ const BlogDetails = (props) => {
   const location = useLocation()
 
   return (
-    <>
+  <>
       <h1>Blog Details</h1>
-      <h2>{location.state.title}</h2>
-      <h3>Written by:  {location.state.author.name}</h3>
-      <p>{location.state.content}</p>
-      {props.user.profile === location.state.author._id && <><Link className="btn btn-warning" state={location.state} to="/editBlog">Edit</Link><br/><br/></>}
-      {location.state.comments.length ? 
-        <>
+
+<section class=" min-h-0 mb-20"> 
+<h1 class="text-center text-white text-xl m-5">Blog DETAIL PAGE </h1> 
+<section class="text-center"> 
+<div class="container text-gray-800 font-semibold mx-auto w-max bg-white  rounded-lg p-3 shadow-2xl  ">
+  <div class="row">
+      <div class="card-body text-center lg:m-5">
+        <h1 class="text-xl text-center">{location.state.title}</h1>
+   
+        <p class="card-text mt-3 mb-3 flex-wrap overflow-y-auto lg:overflow-y-auto md:overflow-y-auto sm:overflow-y-auto">{location.state.content}</p>
+        <p class=" ">Written By: {location.state.author.name} </p> 
+        <p class=" text-sm"> Created On: </p>
+      </div>
+      <div class="text-black text-center m-5">
+      {props.user.profile === location.state.author._id && <>
+      <Link className="py-2 px-4 mr-2 bg-white text-black hover:bg-red-100 focus:ring-red-500 focus:ring-offset-indigo-200 text-red-500 w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg" state={location.state} to="/editBlog">Edit</Link></>}
+  
+</div> 
+    </div>
+    </div>
+    </section> 
+</section>
+
+<CommentForm handleAddComment={props.handleAddComment} blogId={location.state._id}/>
+
+    {location.state.comments.length ? 
+      <>
           <h2>Comments:</h2>
           {location.state.comments.map(comment =>
-            <div class='card' key={comment._id}>
-              <div class="card header">
-                Enter Message/Comment Symbol
-              </div>
-              <blockquote class='blockquote mb-1'>
-              <p class="card-body">{comment.content} </p>
-              <footer class="bockquote footer"> Author<cite class="Source Title"> - {comment.author.name}</cite></footer>
-              </blockquote>
+           <div class="bg-gray-800 ">
+           <section class="card-container mx-auto px20 ml-5 flex flex-col  ">
+           <div class="comments flex flex-wrap items-center justify-center">
+             <div class="container mx-auto  px-20 transform  hover:scale-105 transition duration-300 ">
+           
+               <div class=" mb-5 p-8 bg-white rounded-lg shadow-lg relative hover:shadow-3xl transition-shadow duration-600">
+                 <h1 class="text-2xl text-gray-800 font-semibold mb-3"></h1>
+                  <p class="text-gray-600 leading-6 tracking-normal flex flex-wrap overflow-ellipsis">{comment.content} </p> 
+                  <p class="text-gray-600 leading-6 tracking-normal">
+                   Comment was created at .</p>
+             <p class="py-2 px-4 mt-8 text-gray-700 font-">- {comment.author.name}</p>
+             
+                
+               </div>
+             </div>
+           </div>
               
-            </div>  
+           </section>
+           </div>
           )}
           <h2>Add a comment:</h2>
         </>
         :
         <h2>Add a comment:</h2>
-      }
+    }
       
       
-      <CommentForm handleAddComment={props.handleAddComment} blogId={location.state._id}/>
+     
 
       
-    </>
+  </>
   );
 }
  
--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -4,24 +4,31 @@ import { Link } from 'react-router-dom'
 const Blogs = (props) => {
   return (
     <>
-      <h1>Blogs</h1>
-      <div className="row">
+      
+      <h1 class="text-2xl text-center ">Blogs</h1>
         {props.blogs.map(blog =>
-          <div key={blog._id} className="col-sm-4">
-            <div className="card">
-              <div className="card-header">
-                - {blog.author.name}
+      <div className="mt-3  ">
+          <div key={blog._id} className="bg-white  mb-2 p-8 rounded-lg shadow-2xl relative transform  hover:scale-105 transition duration-300 col-span-12 md:mx-10">
+            <div className="flex items-center justify-between">
+              <div className="">
+                <h5 className="text-2xl font-bold text-gray-700 hover:underline">{blog.title}</h5>
+                <p className="mt-2 text-gray-600">{blog.content.substring(0,100)}...</p>
+
+                <div class="flex items-left justify-between mt-4">
+                  <button className='bg-yellow-500 p-2 rounded-md '>
+                    <Link to='/blogDetails'  state={blog}>Details</Link>
+                  </button> 
+                  {(props.user.profile === blog.author._id) && <button onClick={()=> props.handleDeleteBlog(blog._id)} className="bg-red-500 p-2 rounded-md ">Delete</button>}
+                </div>
               </div>
-              <div className="card-body">
-                <h5 className="card-title">{blog.title}</h5>
-                <p className="card-text">{blog.content.substring(0,100)}...</p>
-                <Link to='/blogDetails' className='btn btn-warning' state={blog}>Details</Link>
-                {(props.user.profile === blog.author._id) && <button onClick={()=> props.handleDeleteBlog(blog._id)} className="btn btn-danger">Delete</button>}
-              </div>
+
+              <div><a href="#" class="flex items-center">
+                <h1 class="font-bold text-gray-700 hover:underline">   - {blog.author.name}</h1>
+            </a></div>
             </div>
           </div>
-        )}
       </div>
+        )}
     </>
   );
 }
